02e5cf5dc47420dcf9d8543aec3192b2e3ae35f0
Brandon Manuel <brandon-manuel@live.com>
2026-02-22 11:59:35 -0600

    More reflection improvements

--- Assets/Scripts/mirror_world.gd
+++ Assets/Scripts/mirror_world.gd
@@ -2,17 +2,21 @@ extends CanvasGroup
 @onready var gym: Node2D = $".."
 @onready var gym_floor: TileMapLayer = $"../Gym Floor"
 @onready var player: CharacterBody2D = $"../Player"
+@onready var player_sprite: AnimatedSprite2D = $"../Player/AnimatedSprite2D"
 
 # Mirror location
 var mirror_grid_pos = Vector2i(0, -4)
-var mirror_world_pos: Vector2i = Vector2i(0, 0)
+
+var mirror_world_pos: Vector2 = Vector2.ZERO
 
 var player_reflection: AnimatedSprite2D = null
 var current_animation = ''
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mirror_world_pos = gym_floor.map_to_local(mirror_grid_pos) - Vector2(0, 12)
+	var tile_size = gym_floor.tile_set.tile_size.y
+	mirror_world_pos = gym_floor.map_to_local(mirror_grid_pos) - Vector2(0, tile_size - 0.5)
 	
 	for object in gym.get_children():
 		if object.name != 'Player' and 'reflections' in object.get_groups():
@@ -20,7 +24,7 @@ func _ready() -> void:
 				var reflection = object.duplicate()
 				add_child(reflection)
 				update_reflection_pos(object, reflection)
-				set_meta("source", object)
+				reflection.set_meta("source", object)
 		elif object.name == 'Player':
 			var sprite = object.get_node('AnimatedSprite2D')
 			var reflection = sprite.duplicate()
@@ -28,19 +32,18 @@ func _ready() -> void:
 			player_reflection = reflection
 			player_reflection.self_modulate = Color(.8, .9, .9, 1)
 			update_reflection_pos(sprite, player_reflection)
-			set_meta("source", object)
+			reflection.set_meta("source", object)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player_reflection is AnimatedSprite2D:
-		var sprite = player.get_node('AnimatedSprite2D')
-		update_reflection_pos(sprite, player_reflection)
-		player_reflection.flip_h = sprite.flip_h
+		update_reflection_pos(player_sprite, player_reflection)
+		player_reflection.flip_h = player_sprite.flip_h
 		
 func update_reflection_pos(source: Node2D, reflection: Node2D):
 	var source_y = source.global_position.y
 	if source == player:
-		source_y += player.get_node('AnimatedSprite2D').offset.y
+		source_y += player_sprite.offset.y
 	elif source is Sprite2D and source.centered:
 		source_y -= source.texture.get_size().y # use top edge instead of center
 	var dist = source_y - mirror_world_pos.y
@@ -52,6 +55,9 @@ func update_reflection_pos(source: Node2D, reflection: Node2D):
 	reflection.z_as_relative = false
 
 func _on_player_is_walking(walking_animation) -> void:
+	if player_reflection == null:
+		return
+		
 	if current_animation != walking_animation:
 		if walking_animation is String:
 			var animation = walking_animation.split(' ')
