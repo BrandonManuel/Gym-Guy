695110e36cccd3facc07b5a675da878373e21a2e
Brandon Manuel <brandon-manuel@live.com>
2026-02-22 12:17:49 -0600

    Added interactable shader

--- Assets/Scripts/dumbbell.gd
+++ Assets/Scripts/dumbbell.gd
@@ -2,4 +2,11 @@ extends Sprite2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body, ", entered dumbbell")
+	enable_outline(true)
+	
+	
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	enable_outline(false)
+	
+func enable_outline(enabled: bool) -> void:
+	material.set_shader_parameter("outline_enabled", enabled)
