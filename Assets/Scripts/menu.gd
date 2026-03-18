0c9fd67285144827e6f597fb1400241acdec9d11
Brandon Manuel <brandon-manuel@live.com>
2026-03-17 20:56:04 -0500

    Made Confirmation Dialog into a custom thing that's a little less ugly

--- Assets/Scripts/menu.gd
+++ Assets/Scripts/menu.gd
@@ -1,72 +1,104 @@
 extends Control
 
-@onready var confirmation_dialog: ConfirmationDialog = $ConfirmationDialog
 @onready var start_button: TextureButton = $"VBoxContainer/Start Button"
 @onready var quit_button: TextureButton = $"VBoxContainer/Quit Button"
 @onready var title_music: AudioStreamPlayer = $"Title Music"
+@onready var confirm_quit: Panel = $ConfirmQuit
+@onready var yes_button: TextureButton = $"ConfirmQuit/VBoxContainer/HBoxContainer/Yes Button"
+@onready var no_button: TextureButton = $"ConfirmQuit/VBoxContainer/HBoxContainer/No Button"
 
 const LABEL_NORMAL_Y = -8.0
 const LABEL_PRESSED_Y = -3.0
 
 var start_pressed: bool = false
 var exit_pressed: bool = false
+var yes_pressed: bool = false
+var no_pressed: bool = false
 
 var times_played = 0
 
 func _ready():
-	start_button.button_down.connect(_on_start_button_down)
-	start_button.button_up.connect(_on_start_button_up)
-	start_button.mouse_entered.connect(_on_start_button_mouse_entered)
-	start_button.mouse_exited.connect(_on_start_button_mouse_exited)
+	confirm_quit.visible = false
+
+func on_button_up(button: Control) -> void:
+	button.get_node('RichTextLabel').position.y = LABEL_NORMAL_Y
 	
-	quit_button.button_down.connect(_on_quit_button_down)
-	quit_button.button_up.connect(_on_quit_button_up)
-	quit_button.mouse_entered.connect(_on_quit_button_mouse_entered)
-	quit_button.mouse_exited.connect(_on_quit_button_mouse_exited)
+func on_button_down(button: Control) -> void:
+	button.get_node('RichTextLabel').position.y = LABEL_PRESSED_Y	
 
-func _on_start_button_down():
-	start_button.get_node('RichTextLabel').position.y = LABEL_PRESSED_Y
-	start_pressed = true
-
-func _on_start_button_up():
-	start_button.get_node('RichTextLabel').position.y = LABEL_NORMAL_Y
+func _on_start_button_button_up() -> void:
+	on_button_up(start_button)
 	start_pressed = false
+
+func _on_start_button_button_down() -> void:
+	on_button_down(start_button)
+	start_pressed = true
 
 func _on_start_button_mouse_entered():
 	if start_pressed:
-		start_button.get_node('RichTextLabel').position.y = LABEL_PRESSED_Y
+		on_button_down(start_button)
 	
 func _on_start_button_mouse_exited():
-	start_button.get_node('RichTextLabel').position.y = LABEL_NORMAL_Y
+	on_button_up(start_button)
 	
-func _on_quit_button_down():
-	quit_button.get_node('RichTextLabel').position.y = LABEL_PRESSED_Y
-	start_pressed = true
+func _on_quit_button_pressed() -> void:
+	confirm_quit.visible = true
 
-func _on_quit_button_up():
-	quit_button.get_node('RichTextLabel').position.y = LABEL_NORMAL_Y
-	start_pressed = false
+func _on_quit_button_button_down() -> void:
+	on_button_down(quit_button)
+	exit_pressed = true
+
+func _on_quit_button_button_up() -> void:
+	on_button_up(quit_button)
+	exit_pressed = false
 
 func _on_quit_button_mouse_entered():
-	if start_pressed:
-		quit_button.get_node('RichTextLabel').position.y = LABEL_PRESSED_Y
+	if exit_pressed:
+		on_button_down(quit_button)
 	
 func _on_quit_button_mouse_exited():
-	quit_button.get_node('RichTextLabel').position.y = LABEL_NORMAL_Y
-	
+	on_button_up(quit_button)
 	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	confirmation_dialog.popup_centered()
-
-func _on_confirmation_dialog_confirmed():
-	get_tree().quit()
-
 
 func _on_title_music_finished() -> void:
 	times_played += 1
 	if times_played < 2:
 		title_music.play(0)
+
+func _on_yes_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_no_button_pressed() -> void:
+	confirm_quit.visible = false
+
+func _on_yes_button_button_down() -> void:
+	on_button_down(yes_button)
+	yes_pressed = true
+
+func _on_yes_button_button_up() -> void:
+	on_button_up(yes_button)
+	yes_pressed = false
+
+func _on_yes_button_mouse_entered() -> void:
+	if yes_pressed:
+		on_button_down(yes_button)
+
+func _on_yes_button_mouse_exited() -> void:
+	on_button_up(yes_button)
+
+func _on_no_button_button_down() -> void:
+	on_button_down(no_button)
+	no_pressed = true
+
+func _on_no_button_button_up() -> void:
+	on_button_up(no_button)
+	no_pressed = false
+
+func _on_no_button_mouse_entered() -> void:
+	if no_pressed:
+		on_button_down(no_button)
+
+func _on_no_button_mouse_exited() -> void:
+	on_button_up(no_button)
