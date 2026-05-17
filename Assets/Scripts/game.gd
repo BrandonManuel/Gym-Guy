731647712069c239d2b834010ad8936aeb7eab79
Brandon Manuel <brandon-manuel@live.com>
2026-05-16 20:13:05 -0500

    Added bug fix for non letter presses

--- Assets/Scripts/game.gd
+++ Assets/Scripts/game.gd
@@ -24,6 +24,7 @@ var score_menu = null
 var currentIndex = 0
 var keysToPress = []
 var roundStarted = false
+var init_timer_value: float = 5.0
 var timer_value: float = 5.0
 var tween: Tween = null
 
@@ -82,7 +83,12 @@ func setupLetters():
 		
 	if score >= 20:
 		num_keys = 5
-	
+		
+	if score >= 25:
+		if timer_value > .5:
+			timer_value = init_timer_value - (.25 * (score - 25))
+			tween.tween_property(progress_bar, "value", 0, timer_value)
+		
 	for i in range(num_keys - keyboard_keys.get_child_count()):
 		var instance = letter.instantiate()
 		var not_pressed: Sprite2D = instance.get_node("Polygon2D").get_node('Not Pressed')
@@ -119,57 +125,64 @@ func set_correct_outline(flag: bool):
 	key.enable_correct(flag)
 	
 func _input(event: InputEvent) -> void:
-	if roundStarted and event is InputEventKey and event.is_pressed():
+	if roundStarted and event is InputEventKey:
+		if not is_letter(event):
+			return
+			
 		var letter = char(event.keycode)
-		if !keysToPress.is_empty():
-			var keyToPress = keysToPress[currentIndex]
-			if keyToPress == letter:
-				keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Not Pressed").visible = false
-				keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Pressed").visible = true
-	
-	if roundStarted and event is InputEventKey and event.is_released():
-		var letter = char(event.keycode)
-		if !keysToPress.is_empty():
-			var keyToPress = keysToPress[currentIndex]
-			if keyToPress == letter:
-				keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Not Pressed").visible = true
-				keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Pressed").visible = false
-				set_current_outline(false)
-				set_correct_outline(true)
-				currentIndex += 1
-				sound_effects.stream = audio_list[sfx.CORRECT_LETTER]
-				sound_effects.play()
-				if currentIndex < keysToPress.size():
-					set_current_outline(true)
-				else:
-					roundStarted = false
-					tween.pause()
-					score_menu.increment()
-					player.get_node('Visual').get_node('AnimationPlayer').play('curling')
-					player.is_lifting.emit('curling')
-					sound_effects.stream = audio_list[sfx.LIFT]
+
+		if roundStarted and event is InputEventKey and event.is_pressed():
+			if !keysToPress.is_empty():
+				var keyToPress = keysToPress[currentIndex]
+				if keyToPress == letter:
+					keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Not Pressed").visible = false
+					keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Pressed").visible = true
+		
+		if roundStarted and event is InputEventKey and event.is_released():
+			if !keysToPress.is_empty():
+				var keyToPress = keysToPress[currentIndex]
+				if keyToPress == letter:
+					keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Not Pressed").visible = true
+					keyboard_keys.get_child(currentIndex).get_node("Polygon2D").get_node("Pressed").visible = false
+					set_current_outline(false)
+					set_correct_outline(true)
+					currentIndex += 1
+					sound_effects.stream = audio_list[sfx.CORRECT_LETTER]
 					sound_effects.play()
-					await get_tree().create_timer(1.4667).timeout
-					for n in keyboard_keys.get_children():
-						keyboard_keys.remove_child(n)
-						n.queue_free()
+					if currentIndex < keysToPress.size():
+						set_current_outline(true)
+					else:
+						roundStarted = false
+						tween.pause()
+						score_menu.increment()
+						player.get_node('Visual').get_node('AnimationPlayer').play('curling')
+						player.is_lifting.emit('curling')
+						sound_effects.stream = audio_list[sfx.LIFT]
+						sound_effects.play()
+						await get_tree().create_timer(1.4667).timeout
+						for n in keyboard_keys.get_children():
+							keyboard_keys.remove_child(n)
+							n.queue_free()
+							
+						keysToPress = []
+						currentIndex = 0
+						setupLetters()
 						
-					keysToPress = []
-					currentIndex = 0
-					setupLetters()
-					
-					progress_bar.value = 100
-					timer_value = 5.0
-					tween = create_tween()
-					tween.tween_property(progress_bar, "value", 0, timer_value)
-					roundStarted = true
-					player.is_walking.emit('idle (back)')
-			else:
-				num_strikes += 1
-				sound_effects.stream = audio_list[sfx.INCORRECT_LETTER]
-				sound_effects.play()
-				var strike_instance = strike.instantiate()
-				strike_instance.custom_minimum_size = Vector2(16, 16)
-				x_container.add_child(strike_instance)
-				if num_strikes >= num_allowed_strikes:
-					end_game()
+						progress_bar.value = 100
+						tween = create_tween()
+						tween.tween_property(progress_bar, "value", 0, timer_value)
+						roundStarted = true
+						player.is_walking.emit('idle (back)')
+				else:
+					num_strikes += 1
+					sound_effects.stream = audio_list[sfx.INCORRECT_LETTER]
+					sound_effects.play()
+					var strike_instance = strike.instantiate()
+					strike_instance.custom_minimum_size = Vector2(16, 16)
+					x_container.add_child(strike_instance)
+					if num_strikes >= num_allowed_strikes:
+						end_game()
+
+func is_letter(event: InputEvent) -> bool:
+	var code = event.keycode
+	return (code >= KEY_A and code <= KEY_Z)
